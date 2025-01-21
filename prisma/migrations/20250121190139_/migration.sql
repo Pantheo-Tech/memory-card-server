b305cff0ee4867e719a0bee1e767d5123178c476
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-01-21 16:02:14 -0300

    nest config

---
+++ prisma/migrations/20250121190139_/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "MemoryCard" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MemoryCard_pkey" PRIMARY KEY ("id")
+);
