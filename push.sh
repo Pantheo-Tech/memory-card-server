9f0756ccfbdb91e1a5b0a3ca07878298fb2a4a96
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-01-29 15:38:28 -0300

    test: teste de commit

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Digite o tipo de commit que deseja fazer:"
+read commit_type
+while [ "$commit_type" == "" ]; do
+    echo "Digite o tipo de commit que deseja fazer:"
+    read commit_type
+done
+echo "Digite a mensagem do commit:"
+read commit_message
+
+while [ "$commit_message" == "" ]; do
+    echo "Digite a mensagem do commit:"
+    read commit_message
+done
+
+current_branch=$(git branch --show-current)
+
+git add . && git commit -m "$commit_type: $commit_message" && git push origin "$current_branch"
+
+echo "Press any key to exit"
+read
