39b081ab8222ac72b06826323d2913509bd125f4
fernando-castelo <nando.castelobranco24@hotmail.com>
2025-02-02 17:44:21 -0300

    feat: add create game route

--- src/controllers/game/game.controller.ts
+++ src/controllers/game/game.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GameDto } from 'src/DTO/game.dto';
+import { RegisterGameDto } from 'src/DTO/register.game.dto';
+import { GameService } from 'src/service/game/game.service';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+  constructor(private readonly gameService: GameService) {}
+
+  @Post()
+  async createGame(@Body() registerGameDto: RegisterGameDto): Promise<GameDto> {
+    return this.gameService.createGame(registerGameDto);
+  }
+}
---
+++ src/service/game/game.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterGameDto } from 'src/DTO/register.game.dto';
+import { GameDto } from 'src/DTO/game.dto';
+
+@Injectable()
+export class GameService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createGame(dto: RegisterGameDto): Promise<GameDto> {
+    const { user_id, ...rest } = dto;
+
+    const userById = await this.prisma.tb_user.findUnique({
+      where: { id: user_id },
+    });
+
+    if (!userById) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const game = await this.prisma.tb_played_games.create({
+      data: {
+        user_id: userById.id,
+        ...rest,
+      },
+    });
+
+    return {
+      id: game.id,
+      platinum: game.platinum,
+      achievements: game.achievements,
+      status: game.status,
+      game_id: game.game_id,
+      user_id: game.user_id,
+      created_at: game.created_at,
+      updated_at: game.updated_at,
+      deleted_at: game.deleted_at,
+    };
+  }
+}
